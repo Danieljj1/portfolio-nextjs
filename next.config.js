39adcbec9af6a7db71c538af04b9a5ff59cf449c
Danieljj1 <danieljjh2003@gmail.com>
2025-12-07 04:00:43 -0500

    Disable ESLint during Next.js build

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true, // enabled react-strict mode
 
   images: {
-    domains: [
-      'i.ibb.co',
-    ],
+    domains: ["i.ibb.co"],
   },
-
 };
 
 module.exports = nextConfig;
